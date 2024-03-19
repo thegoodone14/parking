e2e0515a0cedbe3c94c6b54ca58523e694014ce5
thegoodone14 <112936691+thegoodone14@users.noreply.github.com>
2024-03-19 16:00:54 +0100

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,10 +10,11 @@
         <div class="col-md-6 mb-3">
             <a href="{{ route('reservations.create') }}" class="btn btn-primary btn-lg btn-block">Demander une nouvelle réservation</a>
         </div>
+
         <div class="col-md-6 mb-3">
             <a href="{{ route('reservations.index') }}" class="btn btn-primary btn-lg btn-block">Réservation en cours</a>
         </div>
-    </div>
+        </div>
     <div class="row">
         <div class="col-md-6 mb-3">
             <a href="{{ route('waitlist') }}" class="btn btn-primary btn-lg btn-block">Liste d'attente</a>
