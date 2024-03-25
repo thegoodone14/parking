969d56878d74447fa62bca550adeadd1eeb6199a
thegoodone14 <112936691+thegoodone14@users.noreply.github.com>
2024-03-25 09:12:55 +0000

    Update menu_utilisateur.blade.php

--- resources/views/menu_utilisateur.blade.php
+++ resources/views/menu_utilisateur.blade.php
@@ -1,29 +1,24 @@
-<?php
-//@extends('layouts.app')
+{{-- menu_utilisateur.blade.php --}}
 
-//@section('content')
-echo'<!DOCTYPE html>'
-echo'<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">'
-echo'<div class="container text-center">'
-    echo'<h1 class="my-4">PARKING</h1>'
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<div class="container text-center">
+    <h1 class="my-4">PARKING</h1>
 
-    echo'<div class="row">'
-        echo'<div class="col-md-6 mb-3">'
-            echo'<a href="{{ route('reservations.create') }}" class="btn btn-primary btn-lg btn-block">Demander une nouvelle réservation</a>'
-        echo'</div>'
-
-echo'        div class="col-md-6 mb-3">'
-echo'         <a href="{{ route('reservations.index') }}" class="btn btn-primary btn-lg btn-block">Réservation en cours</a>' 
-       echo ' </div>'
-    echo '</div>'
-    echo '<div class="row">'
-     echo    '<div class="col-md-6 mb-3">'
-           echo '<a href="{{ route('waitlist') }}" class="btn btn-primary btn-lg btn-block">Liste dattente</a>'
-                   echo'</div>'
-    echo'</div>'
-echo'</div>'
-echo '</html>';
-//@endsection
+    <div class="row">
+        <div class="col-md-6 mb-3">
+            <a href="{{ route('reservations.create') }}" class="btn btn-primary btn-lg btn-block">Demander une nouvelle réservation</a>
+        </div>
+        <div class="col-md-6 mb-3">
+            <a href="{{ route('reservations.index') }}" class="btn btn-primary btn-lg btn-block">Réservation en cours</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6 mb-3">
+            <a href="{{ route('waitlist') }}" class="btn btn-primary btn-lg btn-block">Liste d'attente</a>
+        </div>
+    </div>
+</div>
+</html>
 
 
-?>
