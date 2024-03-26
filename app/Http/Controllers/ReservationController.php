845468d72406d8f0dc9e551e85910c588356a760
thegoodone14 <112936691+thegoodone14@users.noreply.github.com>
2024-03-26 12:31:02 +0000

    Update ReservationController.php

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Reservation;
 use App\Models\Place;
+use Illuminate\Support\Facades\DB;
 
 class ReservationController extends Controller
 {
