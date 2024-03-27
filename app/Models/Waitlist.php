fb71c7abec153355a1a304399b9588f5d42ceea9
thegoodone14 <112936691+thegoodone14@users.noreply.github.com>
2024-03-27 13:33:55 +0000

    Update Waitlist.php

--- app/Models/Waitlist.php
+++ app/Models/Waitlist.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Waitlist extends Model
 {
     use HasFactory;
+    protected $table = 'waitlist';
+    public $timestamps = false; // Si vous ne gérez pas les timestamps updated_at
+
+    protected $fillable = ['user_id']; // Les champs que vous pouvez assigner massivement
+
+    // Relation avec le modèle User
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
