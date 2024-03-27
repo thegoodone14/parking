af479a5892f35b8897c72be7a06477d34c2a3b31
thegoodone14 <112936691+thegoodone14@users.noreply.github.com>
2024-03-27 13:41:37 +0000

    Update ReservationController.php

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Reservation;
 use App\Models\Place;
+use App\Models\Waitlist;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth; // Importer la classe Auth
 
